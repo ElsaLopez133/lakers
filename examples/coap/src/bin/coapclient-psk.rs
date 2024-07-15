1a793adf9af775fc4a77d4517824b4c02ffede50
Elsa Lopez Perez <elsa.lopez-perez@inria.fr>
2024-07-15 16:43:57 +0200

    examples: coapserver-psk and coapclient-psk Updating message_2 and message_3 on client and server

--- examples/coap/src/bin/coapclient-psk.rs
+++ examples/coap/src/bin/coapclient-psk.rs
@@ -48,7 +48,7 @@ fn client_handshake() -> Result<(), EDHOCError> {
 
     let message_2 = EdhocMessageBuffer::new_from_slice(&response.message.payload[..]).unwrap();
     let (mut initiator, c_r, id_cred_r, _ead_2) = initiator.parse_message_2(&message_2)?;
-    let valid_cred_r = credential_check_or_fetch(Some(cred), id_cred_r).unwrap();
+    let valid_cred_r = credential_check_or_fetch(Some(cred), id_cred_r.unwrap()).unwrap();
     let initiator = initiator.verify_message_2(valid_cred_r)?;
 
     let mut msg_3 = Vec::from(c_r.as_cbor());
