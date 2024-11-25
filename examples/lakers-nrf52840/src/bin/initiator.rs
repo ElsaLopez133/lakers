ba862e8cf53582d298d19687a4be68767f0aafd1
Elsa Lopez Perez <elsa.lopez-perez@inria.fr>
2024-11-25 11:57:29 +0100

    examples: changing gpios

--- examples/lakers-nrf52840/src/bin/initiator.rs
+++ examples/lakers-nrf52840/src/bin/initiator.rs
@@ -43,14 +43,18 @@ async fn main(spawner: Spawner) {
     let p1 = nrf52840_hal::gpio::p1::Parts::new(peripherals.P1);
 
     let mut led_pin_p0_26 = p0.p0_26.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
-    let mut led_pin_p0_8 = p0.p0_08.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
-    let mut led_pin_p0_7 = p0.p0_07.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
-    let mut led_pin_p0_6 = p0.p0_06.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
-    let mut led_pin_p0_5 = p0.p0_05.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    // let mut led_pin_p0_8 = p0.p0_08.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    // let mut led_pin_p0_7 = p0.p0_07.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    // let mut led_pin_p0_6 = p0.p0_06.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    // let mut led_pin_p0_5 = p0.p0_05.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
 
     let mut led_pin_p1_07 = p1.p1_07.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
     let mut led_pin_p1_08 = p1.p1_08.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
     let mut led_pin_p1_06 = p1.p1_06.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    let mut led_pin_p1_05 = p1.p1_05.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+    let mut led_pin_p1_04 = p1.p1_04.into_push_pull_output(nrf52840_hal::gpio::Level::Low); // used for the whole message
+    let mut led_pin_p1_10 = p1.p1_10.into_push_pull_output(nrf52840_hal::gpio::Level::Low);
+
 
     let mut config = embassy_nrf::config::Config::default();
     config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
@@ -86,30 +90,40 @@ async fn main(spawner: Spawner) {
 
         // info!("Prepare message_1");
         led_pin_p0_26.set_high();
+        led_pin_p1_04.set_high();
+        led_pin_p1_07.set_high();
         let cred_i: Credential = Credential::parse_ccs_symmetric(common::CRED_PSK.try_into().unwrap()).unwrap();
         let cred_r: Credential = Credential::parse_ccs_symmetric(common::CRED_PSK.try_into().unwrap()).unwrap();
+        led_pin_p1_07.set_low();
         //info!("cred_r:{:?}", cred_r.bytes.content);
         
+        led_pin_p1_07.set_high();
         let mut initiator = EdhocInitiator::new(
             lakers_crypto::default_crypto(),
             EDHOCMethod::PSK2,
             EDHOCSuite::CipherSuite2,
         );
+        led_pin_p1_07.set_low();
 
         // Send Message 1 over raw BLE and convert the response to byte
+        led_pin_p1_07.set_high();
         // let c_i = generate_connection_identifier_cbor(&mut lakers_crypto::default_crypto());
         let c_i = ConnId::from_int_raw(10);
-        initiator.set_identity(None, cred_i);
+        led_pin_p1_07.set_low();
 
-        led_pin_p0_6.set_high();
+        led_pin_p1_07.set_high();
+        initiator.set_identity(None, cred_i);
+        led_pin_p1_07.set_low();
+
+        led_pin_p1_07.set_high();
         let (initiator, message_1) = initiator.prepare_message_1(Some(c_i), &None).unwrap();
-        led_pin_p0_6.set_low();
+        led_pin_p1_07.set_low();
 
         let pckt_1 = common::Packet::new_from_slice(message_1.as_slice(), Some(0xf5))
             .expect("Buffer not long enough");
         // info!("Send message_1 and wait message_2");
         led_pin_p0_26.set_low();
-        let rcvd = common::transmit_and_wait_response(&mut radio, pckt_1, Some(0xf5), &mut led_pin_p1_07).await;
+        let rcvd = common::transmit_and_wait_response(&mut radio, pckt_1, Some(0xf5), &mut led_pin_p1_10).await;
 
         match rcvd {
             Ok(pckt_2) => {
@@ -118,37 +132,40 @@ async fn main(spawner: Spawner) {
                 let message_2: EdhocMessageBuffer =
                     pckt_2.pdu[1..pckt_2.len].try_into().expect("wrong length");
 
-                led_pin_p0_5.set_high();
+                led_pin_p1_06.set_high();
                 let (initiator, c_r, id_cred_r, ead_2) = initiator.parse_message_2(&message_2).unwrap();
-                led_pin_p0_5.set_low();
-
+                led_pin_p1_06.set_low();
+                
+                led_pin_p1_06.set_high();
                 let valid_cred_r = credential_check_or_fetch(Some(cred_r), id_cred_r.unwrap()).unwrap();
+                led_pin_p1_06.set_low();
 
-                led_pin_p0_8.set_high();
+                led_pin_p1_06.set_high();
                 let initiator = initiator
                     .verify_message_2(valid_cred_r)
                     .unwrap();
-                led_pin_p0_8.set_low();
+                led_pin_p1_06.set_low();
 
                 led_pin_p0_26.set_low();
 
                 // info!("Prepare message_3");
                 led_pin_p0_26.set_high();
 
-                led_pin_p0_7.set_high();
+                led_pin_p1_08.set_high();
                 let (initiator, message_3, i_prk_out) = initiator
                     .prepare_message_3(CredentialTransfer::ByReference, &None).unwrap();
-                led_pin_p0_7.set_low();
+                led_pin_p1_08.set_low();
                 // info!("Send message_3");
                 led_pin_p0_26.set_low();
                 common::transmit_without_response(
                     &mut radio,
                     common::Packet::new_from_slice(message_3.as_slice(), Some(c_r.as_slice()[0]))
                         .unwrap(),
-                    &mut led_pin_p1_08)
+                    &mut led_pin_p1_10)
                 .await;
                 
                 info!("Handshake completed. prk_out = {:X}", i_prk_out);
+                led_pin_p1_04.set_low();
             }
             // Err(_) => panic!("parsing error"),
             // Added to measure time. Otherwise revert to up.
@@ -162,3 +179,4 @@ async fn main(spawner: Spawner) {
     info!("start time: {:?} and elapsed time in ms: {:?}", start, duration.as_millis());
     info!("duration of one handshake in ms: {:?}", duration.as_millis()/100);
 }
+
