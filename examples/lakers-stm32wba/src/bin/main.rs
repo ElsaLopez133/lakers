29b439b142679008ca91749b2275247a1bf327ae
Elsa Lopez Perez <elsa.lopez-perez@inria.fr>
2025-04-02 15:04:45 +0200

    NEEDS FIX: error compiling lakers-stm32wba. defmt error

---
+++ examples/lakers-stm32wba/src/bin/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+// Reference Manual: file:///C:/Users/elopezpe/OneDrive/Documentos/PhD/micro/stm32eba55cg/rm0493-multiprotocol-wireless-bluetooth-low-energy-and-ieee802154-stm32wba5xxx-arm-based-32-bit-mcus-stmicroelectronics-en.pdf
+
+use stm32wba::stm32wba55;
+use {defmt_rtt as _, panic_probe as _};
+use cortex_m_rt::entry;
+use cortex_m::asm;
+// use defmt::info;
+
+#[entry]
+unsafe fn main() -> ! {
+    // Access peripherals via PAC
+    let p = stm32wba55::Peripherals::take().unwrap();
+    
+    // call lakers-crypto-rustcrypto-stm private init function
+
+    lakers_crypto_rustcrypto_stm_init(p);
+
+    // call lakers prepare_ead_1
+    // call lakers prepare_message_1
+
+    loop {}
+}
